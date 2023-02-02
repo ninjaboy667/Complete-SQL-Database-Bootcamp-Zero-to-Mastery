9ae500ff44fdc32ab59f656f6879bbe4b50d6d79
Eli Barber <46325011+ninjaboy667@users.noreply.github.com>
2023-02-01 22:18:05 -0700

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -9,9 +9,11 @@
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
 
+ANSWER: 63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
+Answer: 1965-02-01
 
 
 -- BEFORE YOU START
@@ -23,6 +25,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
+ANSWER: 36684
 
 
 -- BEFORE YOU START
@@ -34,11 +37,16 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+select count(countrycode) from countrylanguage
+where isofficial = true;
+234
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
+ANSWER: 66.486036
 
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
+185001.75
 
 
